b05f3d4239671f9780742619dcc7666765971301
Felipe <felipegabrielsilvabiazin@gmail.com>
2024-10-15 13:20:53 -0300

    dia 15-10-2024

--- funcionario.js
+++ funcionario.js
@@ -7,12 +7,50 @@ function Pessoa(nome) {
 
 function Funcionario(nome, cargo, salario) {
     this.cargo = cargo;
-    this.salario = salario;
+    // this.salario = salario;
+    let _salario = salario;
+
+    // getters e setters
+    this.getSalario = function() {
+        return `O salario de ${this.nome} é ${_salario}`;
+    }
+
+    this.setSalario = function(valor) {
+        if (typeof valor === 'number') {
+            _salario = valor;
+        }
+    }
+
+    this.aumento = function() {
+        const novoSalario = _salario * 1.1;
+
+        console.log(novoSalario)
+        _salario = novoSalario;
+    }
+
+    this.dizCargo = function() {
+        console.log(this.cargo);
+    }
+
+    Pessoa.call(this, nome);
 }
 
-const pessoa1 = new Pessoa("Maria");
-const funcionario1 = new Funcionario("Maria", "dev front-end", 5000)
-pessoa1.dizOi();
+function Estagiario(nome) {
+    Funcionario.call(this, nome, "Estagiario", 2000);
 
-console.log(pessoa1);
-console.log(funcionario1);
+    this.aumento = function() {
+        const novoSalario = _salario * 1.07;
+
+        console.log(novoSalario)
+        _salario = novoSalario;
+    }
+}
+
+const funcionario1 = new Funcionario("Maria", "dev front-end", 5000);
+const funcionario2 = new Funcionario("Pedro", "estagiario", 2000);
+
+funcionario1.aumento();
+console.log(funcionario1.getSalario())
+
+funcionario2.aumento();
+console.log(funcionario2.getSalario())
