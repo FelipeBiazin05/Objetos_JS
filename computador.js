00d3abb273dcc6999d2fc01b8a04a27c7df9112e
Felipe <felipegabrielsilvabiazin@gmail.com>
2024-10-17 07:06:11 -0300

    Adicionando mais uma instancia

--- computador.js
+++ computador.js
@@ -6,7 +6,7 @@ function Computador(desktop, notebook) {
     }
 }
 
-function ComfiguracaoDesktop (CPU, GPU, MemoriaRAM, armazenamento, teclado, mouse) {
+function ConfiguracaoDesktop (CPU, GPU, MemoriaRAM, armazenamento, teclado, mouse) {
     this.CPU = CPU;
     this.GPU = GPU;
     this.MemoriaRAM = MemoriaRAM;
@@ -23,8 +23,12 @@ function ConfiguracaoNotebook (CPU, GPU, MemoriaRAM, armazenamento, touchpad) {
     this.touchpad = touchpad;
 }
 
-const computadorDoFelipe = new ComfiguracaoDesktop ("Xeon", "Rtx 580", "HyperX 32gb", "SDD 1tb", "HP", "Logitech" );
+const computadorDoFelipe = new ConfiguracaoDesktop ("Xeon", "Rtx 580", "HyperX 32gb", "SDD 1tb", "HP", "Logitech" );
 const notebookDeMaria = new ConfiguracaoNotebook ("Intel i3", "Integrada 2gb", "4gb", "HD 500gb", true);
+const computadorDoCarlos = new ConfiguracaoDesktop("Ryzen 7", "RTX 3060", "Corsair 16GB", "SSD 512GB", "Logitech", "Razer");
+const notebookDaAna = new ConfiguracaoNotebook("Intel i5", "NVIDIA GTX 1650", "8GB", "HD 1TB", true);
 
 console.log(computadorDoFelipe);
 console.log(notebookDeMaria);
+console.log(computadorDoCarlos);
+console.log(notebookDaAna);
